9cc6f062f0661a54444c41f9558f5ee52f428d44
RiceBoyy <owen.j.de.guzman@gmail.com>
2026-03-07 04:31:54 +0100

    Initial Next.js + Tailwind setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#1c0f08] text-white">
+      <h1 className="text-5xl font-bold p-10">
+        Caffe Junior
+      </h1>
+    </main>
+  );
+}
